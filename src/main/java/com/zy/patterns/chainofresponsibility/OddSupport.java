cb94aaa8ffc49b4359aa3c5e713cce54fc74a31f
zhongyuan <zhongyuan966@gmail.com>
2020-04-19 14:38:15 +0800

    Chain of responsibility 模式

---
+++ src/main/java/com/zy/patterns/chainofresponsibility/OddSupport.java
@@ -0,0 +1,17 @@
+package com.zy.patterns.chainofresponsibility;
+
+public class OddSupport extends Support {
+
+    public OddSupport(String name) {
+        super(name);
+    }
+
+    @Override
+    protected boolean resolve(Trouble trouble) {
+        if (trouble.getNumber() % 2 == 1) {
+            return true;
+        }
+
+        return false;
+    }
+}
